4ca7882defff5824e8d3b0390491c2182d835de2
1CASH1 <7905831+1CASH1@users.noreply.github.com>
2026-07-15 00:01:54 -0600

    Fix roles migration dependency order

--- roles/migrations/0002_initial.py
+++ roles/migrations/0002_initial.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ("roles", "0001_initial"),
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ("user", "0002_auth_schema_table"),
     ]
 
     operations = [
