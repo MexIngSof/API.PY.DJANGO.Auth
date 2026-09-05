e2559978cd02788432d8f7230820d850130aae9a
1CASH1 <cash_dip@hotmail.com>
2026-09-05 00:01:53 -0600

    feat(observability): enable ASGI request context

--- config/asgi.py
+++ config/asgi.py
@@ -2,15 +2,14 @@
 ASGI config for auth project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+from .observability import ObservabilityMiddleware
 
-application = get_asgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+application = ObservabilityMiddleware(get_asgi_application())
